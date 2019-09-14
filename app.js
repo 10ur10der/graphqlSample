b373c6e15721e6d6ee4e53f5baf880c876539c70
10ur10der <10ur10der@gmail.com>
2019-09-14 21:47:49 +0300

    Improve SingUp

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Context-Type,Authorization');
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(isAuth);
 
 app.use('/graphql',graphqlHttp({
@@ -26,7 +36,7 @@ const password = process.env.MONGO_PASSWORD;
 const user = process.env.MONGO_USER;
 const db = process.env.MONGO_DB;
 mongoose.connect('mongodb+srv://'+user+':'+password+'@cluster0-lchs0.mongodb.net/'+db+'?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-    app.listen(3000);
+    app.listen(8000);
 }).catch(err=>{
     console.log(err);
 });
